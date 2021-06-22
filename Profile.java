1efe7091ca0f9331e8d45ca121c0e874a11c79bd
Hakiemhalim <86224719+Hakiemhalim@users.noreply.github.com>
2021-06-22 11:16:51 +0800

    Add files via upload

---
+++ Profile.java
@@ -0,0 +1,20 @@
+package com.example.bmicalculatorapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Profile extends AppCompatActivity {
+
+    Button backBtn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
